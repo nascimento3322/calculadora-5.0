cba53a8883a6dd2ec640b1a9111b1bf9c327162f
nascimento3322 <vn62511@gmail.com>
2025-04-29 13:47:59 -0300

    Multiplicando

--- code.py
+++ code.py
@@ -1,8 +1,4 @@
 x = int(input("Digite um número: "))
 y = int(input("Digite outro número: "))
-
-soma = x + y
-print("O resultado da soma é:", soma)
-
-subtracao = x - y
-print("O resultado da subtração é:", subtracao)
+multiplicacao = x * y
+print("O resultado final do calculo é: ",multiplicacao)
